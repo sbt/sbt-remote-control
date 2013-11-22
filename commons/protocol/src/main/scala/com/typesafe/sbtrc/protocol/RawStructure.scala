e0b8e0864d6cf2ff0b7b149459c66318ca6922fd
Josh Suereth <joshua.suereth@gmail.com>
2013-11-22 09:34:29 -0500

    Add documentation to RawStructure.

--- commons/protocol/src/main/scala/com/typesafe/sbtrc/protocol/RawStructure.scala
+++ commons/protocol/src/main/scala/com/typesafe/sbtrc/protocol/RawStructure.scala
@@ -5,8 +5,30 @@ import scala.util.parsing.json._
 import com.typesafe.sbtrc.ipc.JsonReader
 
 
-// Hacky, Hacky typeclass to work with Scala's raw JSON library
-// (so the same code can be used on multiple scala versions, yay).
+/** Hacky, Hacky typeclass to work with Scala's raw JSON library
+ *(so the same code can be used on multiple scala versions, yay).
+ * 
+ *  A RawStructure is a class decomposed into "basic" types supported by json, that is:
+ *  
+ *  Map[String,Any] -> Object
+ *  Seq[Any]        -> Array
+ *  String          -> String
+ *  Int, Double     -> Number
+ *  Boolean         -> Boolean
+ *  null            -> null
+ * 
+ * This is the mechanism whereby we serialize/deserialze from JSON.  We use this
+ * low-level of an API so we can remain JSON library agnostic (and to some extent
+ * serialization format agnostic) and still be able to decompose/reconstitute messages.
+ * 
+ * Hopefully at some point we can migrate into an alternative which is a bit easier to work with.
+ * 
+ * Play's JSON library is quite nice for this sort of thing, but is not source compatible between
+ * Scala 2.10, 2.9 and 2.11.
+ * 
+ * 
+ * TODO - To be "true" to JSON, this should probably create an Any and extract from Any.
+ */
 trait RawStructure[T] {
   def apply(t: T): Map[String, Any]
   def unapply(map: Map[String, Any]): Option[T]
