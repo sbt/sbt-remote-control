e10eb113a08b7f2322dc83f72c45e62f37d1f0fc
Josh Suereth <joshua.suereth@gmail.com>
2014-01-31 06:20:55 -0500

    For now, clean log directory on reboot.

--- server/src/main/scala/com/typesafe/sbtrc/server/SbtServerMain.scala
+++ server/src/main/scala/com/typesafe/sbtrc/server/SbtServerMain.scala
@@ -8,6 +8,9 @@ case class Exit(code: Int) extends xsbti.Exit
 
 class SbtServerMain extends ServerMain {
   def start(config: AppConfiguration): xsbti.Server = {
+    // Clean the log directory first....
+    val file = new java.io.File(".sbtserver")
+    sbt.IO.delete(file);
     val server = new SbtServer(config, new ServerSocket(0))
     server.start()
     server
