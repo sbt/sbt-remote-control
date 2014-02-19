784b12205b1016bb5ead6e97136ae70708dbc636
Josh Suereth <joshua.suereth@gmail.com>
2014-02-19 09:33:03 -0500

    Add task execution and stdout capture to tests.

--- integration-tests/src/main/scala/com/typesafe/sbtrc/it/loading/CanLoadSimpleProject.scala
+++ integration-tests/src/main/scala/com/typesafe/sbtrc/it/loading/CanLoadSimpleProject.scala
@@ -12,6 +12,13 @@ class CanLoadSimpleProject extends SbtClientTest {
   // TODO - Don't hardcode sbt versions, unless we have to...
   val dummy = utils.makeDummySbtProject("test")
 
+  sbt.IO.write(new java.io.File(dummy, "stdout.sbt"),
+
+    """| val printOut = taskKey[Unit]("Print to stdout and see if we get it.")
+       |  
+       | printOut := System.out.println("test-out")
+       |""".stripMargin)
+
   withSbt(dummy) { client =>
     val build = concurrent.promise[MinimalBuildStructure]
     import concurrent.ExecutionContext.Implicits.global
@@ -24,6 +31,16 @@ class CanLoadSimpleProject extends SbtClientTest {
     // Here we check autocompletions:
     val completes = Await.result(client.possibleAutocompletions("hel", 0), defaultTimeout)
     assert(completes.exists(_.append == "p"), "Failed to autocomplete `help` command.")
+
+    // Attempt to call a custom task and ensure we get our stdout events.
+    val stdoutCaptured = concurrent.promise[Unit]
+    val stdoutSub = (client handleEvents {
+      case LogEvent(LogStdOut(line)) if line contains "test-out" =>
+        stdoutCaptured.success(())
+      case _ =>
+    })(global)
+    client.requestExecution("printOut", None)
+    Await.result(stdoutCaptured.future, defaultTimeout)
   }
 
 }
