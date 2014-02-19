e2c66e12340e1007be6b066c6e781f0a064a3475
Josh Suereth <joshua.suereth@gmail.com>
2014-02-19 09:43:37 -0500

    Improve error messages for integration test Await blocks.
    Also add log.info capture tests.

--- integration-tests/src/main/scala/com/typesafe/sbtrc/it/loading/CanLoadSimpleProject.scala
+++ integration-tests/src/main/scala/com/typesafe/sbtrc/it/loading/CanLoadSimpleProject.scala
@@ -16,31 +16,44 @@ class CanLoadSimpleProject extends SbtClientTest {
 
     """| val printOut = taskKey[Unit]("Print to stdout and see if we get it.")
        |  
-       | printOut := System.out.println("test-out")
+       | printOut := {
+       |   System.out.println("test-out")
+       |   System.err.println("test-err")
+       |   streams.value.log.info("test-info")
+       | }
        |""".stripMargin)
 
   withSbt(dummy) { client =>
     val build = concurrent.promise[MinimalBuildStructure]
     import concurrent.ExecutionContext.Implicits.global
     client watchBuild build.success
-    val result = Await.result(build.future, defaultTimeout)
+    val result = waitWithError(build.future, "Never got build structure.")
     assert(result.projects.size == 1, "Found too many projects!")
     val project = result.projects.head
     assert(project.name == "test", "failed to discover project name == file name.")
 
     // Here we check autocompletions:
-    val completes = Await.result(client.possibleAutocompletions("hel", 0), defaultTimeout)
+    val completes = waitWithError(client.possibleAutocompletions("hel", 0), "Autocompletions not returned in time.")
     assert(completes.exists(_.append == "p"), "Failed to autocomplete `help` command.")
 
     // Attempt to call a custom task and ensure we get our stdout events.
     val stdoutCaptured = concurrent.promise[Unit]
+    val logInfoCaptured = concurrent.promise[Unit]
+    val stderrCaptured = concurrent.promise[Unit]
     val stdoutSub = (client handleEvents {
       case LogEvent(LogStdOut(line)) if line contains "test-out" =>
         stdoutCaptured.success(())
+      case LogEvent(LogStdErr(line)) if line contains "test-err" =>
+        stderrCaptured.success(())
+      case LogEvent(LogMessage("info", line)) if line contains "test-info" =>
+        logInfoCaptured.success(())
       case _ =>
     })(global)
     client.requestExecution("printOut", None)
-    Await.result(stdoutCaptured.future, defaultTimeout)
+    // Now we wait for the futures to fill out.
+    waitWithError(stdoutCaptured.future, "Unable to read known stdout lines from server")
+    waitWithError(stderrCaptured.future, "Unable to read known stderr lines from server")
+    waitWithError(logInfoCaptured.future, "Unable to read known log info lines from server")
   }
 
 }
