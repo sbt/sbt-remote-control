b3d2f4fdc10ab81e6f6b80b18da66c43128bc9dd
Havoc Pennington <hp@pobox.com>
2014-04-10 21:06:25 -0400

    Use a serialized executor to fix SimpleConnector close
    We were depending on the order (open first, close second) but
    throwing callbacks into the default Scala executor which does
    not guarantee any order. Switch to a custom executor which
    serializes everything. We can't just use same thread executor
    because one of these callbacks joins the thread that would
    run the callback.

---
+++ client/src/test/scala/com/typesafe/sbtrc/client/ExecutionContextsTest.scala
@@ -0,0 +1,27 @@
+/**
+ *   Copyright (C) 2014 Typesafe Inc. <http://typesafe.com>
+ */
+package com.typesafe.sbtrc.client
+
+import org.junit.Assert._
+import org.junit._
+
+class ExecutionContextsTest {
+
+  @Test
+  def testSerializedCached(): Unit = {
+    val ctx = ExecutionContexts.serializedCachedExecutionContext
+    def execute(f: => Unit): Unit = {
+      ctx.execute(new Runnable { override def run() = f })
+    }
+    // supposed to run one thing at a time, in order.
+    @volatile var items = Vector.empty[Int]
+    for (v <- 1 to 100)
+      execute(items :+= v)
+    val latch = new java.util.concurrent.CountDownLatch(1)
+    execute(latch.countDown())
+    assertEquals(100, items.size)
+    for (v <- 1 to 100)
+      assertEquals(v, items(v - 1))
+  }
+}
