1b61ac3da12cf6f5b8e2951975efe59aa03ee2ce
Havoc Pennington <hp@pobox.com>
2014-08-13 22:27:31 -0400

    On startup rename master.log to previous.log
    This keeps us from immediately losing master.log just
    because the server restarted.

--- server/src/main/scala/com/typesafe/sbtrc/server/SbtServerMain.scala
+++ server/src/main/scala/com/typesafe/sbtrc/server/SbtServerMain.scala
@@ -10,8 +10,16 @@ class SbtServerMain extends ServerMain {
   def start(config: AppConfiguration): xsbti.Server = {
     System.err.println(s"Starting sbt server...")
     // Clean the log directory first....
-    val file = new java.io.File(".sbtserver")
-    sbt.IO.delete(file);
+    val serverDir = new java.io.File(".sbtserver")
+    // for debugging, it's useful to save the old log file
+    val oldLogFile = new java.io.File(serverDir, "master.log")
+    val previousLogFile = new java.io.File(serverDir, "previous.log")
+    if (oldLogFile.exists)
+      sbt.IO.move(oldLogFile, previousLogFile)
+    for (f <- sbt.IO.listFiles(serverDir)) {
+      if (f.getName != "previous.log")
+        sbt.IO.delete(f)
+    }
     new SbtServer(config, new ServerSocket(0))
   }
 }
