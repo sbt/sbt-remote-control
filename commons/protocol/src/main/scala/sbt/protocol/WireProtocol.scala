7506af47943d8fd4e769e9653a410e52839411fe
Havoc Pennington <hp@pobox.com>
2015-01-12 16:46:36 -0500

    Fix Envelope.apply to parse messages

--- commons/protocol/src/main/scala/sbt/protocol/WireProtocol.scala
+++ commons/protocol/src/main/scala/sbt/protocol/WireProtocol.scala
@@ -3,6 +3,8 @@ package sbt.protocol
 import sbt.impl.ipc
 import java.io.File
 import language.existentials
+import scala.util.control.NonFatal
+import sbt.serialization._
 
 private[sbt] final case class Envelope(override val serial: Long, override val replyTo: Long, override val content: Message) extends ipc.Envelope[Message]
 
@@ -12,8 +14,10 @@ private[sbt] final case class Envelope(override val serial: Long, override val r
  */
 private[sbt] object Envelope {
   def apply(wire: ipc.WireEnvelope): Envelope = {
-    val message: Message = // FIXME
-      Some((???).asInstanceOf[Message]).getOrElse(ErrorResponse("exception parsing json"))
+    val message: Message =
+      JsonValue.parseJson(wire.asString).flatMap(_.parse[Message]).recover({
+        case NonFatal(e) => ErrorResponse(s"exception parsing json: ${e.getMessage}")
+      }).get
 
     new Envelope(wire.serial, wire.replyTo, message)
   }
