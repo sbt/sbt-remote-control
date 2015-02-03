5c2f96f6f4ad2113e812a20cc2e9f718b3cfb008
Havoc Pennington <hp@pobox.com>
2015-02-03 15:02:07 -0500

    Drop StackTraceElementDeserialized - not necessary
    This means the generated json has $type java.lang.StackTraceElement
    instead of the ugly/private StackTraceElementDeserialized.
    Also, declaringClass (which was the constructor parameter name)
    changed to className (the actual field name).

--- serialization/src/main/scala/sbt/serialization/pickler/Throwable.scala
+++ serialization/src/main/scala/sbt/serialization/pickler/Throwable.scala
@@ -3,17 +3,45 @@ package pickler
 
 // TODO - Why is alias not working.
 import scala.pickling.pickler.{ PrimitivePicklers, RefPicklers }
-import scala.pickling.{ FastTypeTag, PBuilder, PReader, PicklingException }
-
-private[pickler] final case class StackTraceElementDeserialized(declaringClass: String,
-  methodName: String,
-  fileName: String,
-  lineNumber: Int)
+import scala.pickling.PicklingException
 
 trait ThrowablePicklers extends PrimitivePicklers with OptionPicklers with VectorPicklers with RefPicklers {
 
-  implicit val stackTracePickler: SPickler[StackTraceElementDeserialized] = SPickler.generate[StackTraceElementDeserialized]
-  implicit val stackTraceUnickler: Unpickler[StackTraceElementDeserialized] = Unpickler.generate[StackTraceElementDeserialized]
+  private implicit object stackTracePickler extends SPicklerUnpickler[StackTraceElement] {
+    override val tag: FastTypeTag[StackTraceElement] = implicitly[FastTypeTag[StackTraceElement]]
+    private val intTag = implicitly[FastTypeTag[Int]]
+    private val stringOptTag = implicitly[FastTypeTag[Option[String]]]
+    private val stringOptPickler = implicitly[SPickler[Option[String]]]
+    private val stringOptUnpickler = implicitly[Unpickler[Option[String]]]
+
+    override def pickle(a: StackTraceElement, builder: PBuilder): Unit = {
+      builder.beginEntry(a)
+      def pickleString(field: String, value: String): Unit = {
+        builder.putField(field, { b =>
+          b.hintTag(stringOptTag)
+          stringOptPickler.pickle(Option(value), b)
+        })
+      }
+      pickleString("className", a.getClassName)
+      pickleString("methodName", a.getMethodName)
+      pickleString("fileName", a.getFileName)
+      builder.putField("lineNumber", { b =>
+        b.hintTag(intTag)
+        intPickler.pickle(a.getLineNumber, b)
+      })
+      builder.endEntry()
+    }
+    override def unpickle(tag: String, preader: PReader): StackTraceElement = {
+      def unpickleString(field: String): Option[String] = {
+        stringOptUnpickler.unpickleEntry(preader.readField(field)).asInstanceOf[Option[String]]
+      }
+      val className = unpickleString("className")
+      val methodName = unpickleString("methodName")
+      val fileName = unpickleString("fileName")
+      val lineNumber = intPickler.unpickleEntry(preader.readField("lineNumber")).asInstanceOf[Int]
+      new StackTraceElement(className.orNull, methodName.orNull, fileName.orNull, lineNumber)
+    }
+  }
 
   // TODO why isn't this in LowPriority / what goes in Low and what goes here?
   implicit object throwablePicklerUnpickler extends SPickler[Throwable] with Unpickler[Throwable] {
@@ -24,11 +52,9 @@ trait ThrowablePicklers extends PrimitivePicklers with OptionPicklers with Vecto
     private val stringOptPickler = implicitly[SPickler[Option[String]]]
     private val stringOptUnpickler = implicitly[Unpickler[Option[String]]]
     private val throwableOptPicklerUnpickler = optionPickler[Throwable](tag, this, this, throwableOptTag)
-    private val vstedTag = implicitly[FastTypeTag[Vector[StackTraceElementDeserialized]]]
-    private val vstedPickler = vectorPickler[StackTraceElementDeserialized]
-
-    //implicitly[SPickler[Vector[StackTraceElementDeserialized]]]
-    private val vstedUnpickler = vstedPickler
+    private val vsteTag = implicitly[FastTypeTag[Vector[StackTraceElement]]]
+    private val vstePickler = vectorPickler[StackTraceElement]
+    private val vsteUnpickler = vstePickler
 
     def pickle(a: Throwable, builder: PBuilder): Unit = {
       builder.beginEntry(a)
@@ -41,10 +67,8 @@ trait ThrowablePicklers extends PrimitivePicklers with OptionPicklers with Vecto
         throwableOptPicklerUnpickler.pickle(Option(a.getCause), b)
       })
       builder.putField("stackTrace", { b =>
-        b.hintTag(vstedTag)
-        vstedPickler.pickle(a.getStackTrace.toVector map { x =>
-          StackTraceElementDeserialized(x.getClassName, x.getMethodName, x.getFileName, x.getLineNumber)
-        }, b)
+        b.hintTag(vsteTag)
+        vstePickler.pickle(a.getStackTrace.toVector, b)
       })
       builder.endEntry()
     }
@@ -52,11 +76,9 @@ trait ThrowablePicklers extends PrimitivePicklers with OptionPicklers with Vecto
       val message = stringOptUnpickler.unpickleEntry(preader.readField("message")).asInstanceOf[Option[String]]
       val cause = throwableOptPicklerUnpickler.unpickleEntry(preader.readField("cause")).asInstanceOf[Option[Throwable]]
       preader.hintStaticallyElidedType()
-      val stackTrace = vstedUnpickler.unpickleEntry(preader.readField("stackTrace")).asInstanceOf[Vector[StackTraceElementDeserialized]]
+      val stackTrace = vsteUnpickler.unpickleEntry(preader.readField("stackTrace")).asInstanceOf[Vector[StackTraceElement]]
       val result = new Exception(message.orNull, cause.orNull)
-      result.setStackTrace((stackTrace map { x =>
-        new StackTraceElement(x.declaringClass, x.methodName, x.fileName, x.lineNumber)
-      }).toArray)
+      result.setStackTrace(stackTrace.toArray)
       result
     }
   }
